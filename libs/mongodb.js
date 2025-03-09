24bbd496962fdddb1e8097df3b5732783186a8d9
banovshaa <banovshaea@code.edu.az>
2025-03-09 23:11:42 +0400

    mongo db upd

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,11 +1,29 @@
 import mongoose from "mongoose";
 
+// const connectMongoDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("connected to Mongo DB");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("connected to Mongo DB");
+    if (mongoose.connections[0].readyState) {
+      console.log("Already connected to the database.");
+      return;
+    }
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to the database.");
   } catch (error) {
-    console.log(error);
+    console.error("Database connection error:", error);
+    throw error;
   }
 };
 
