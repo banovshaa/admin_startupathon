d71af8d1f1c614d678a98e8909d8088a1b6ca8fe
banovshaa <banovshaea@code.edu.az>
2025-03-10 20:21:04 +0400

    Cors added

--- app/api/completers/route.js
+++ app/api/completers/route.js
@@ -33,5 +33,21 @@ export async function POST(request) {
 export async function GET() {
   await connectMongoDB();
   const completers = await Completer.find();
-  return NextResponse.json({ completers });
+  const response = NextResponse.json({ completers });
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+
+  return response;
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
 }
